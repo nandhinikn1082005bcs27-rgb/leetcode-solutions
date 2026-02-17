dd373b0957aed6d00c76575df68d841bf474a7b7
nandhinikn1082005bcs27-rgb <nandhinikn1082005@gmail.com>
2026-02-17 12:35:22 +0530

    Create lon_valid_paren.py

---
+++ lon_valid_paren.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack, max_len = [-1], 0
+        for i, char in enumerate(s):
+            if char == '(': stack.append(i)
+            else:
+                stack.pop()
+                if not stack: stack.append(i)
+                else: max_len = max(max_len, i - stack[-1])
+        return max_len
