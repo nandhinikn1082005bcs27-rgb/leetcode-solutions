17f30253f05a6a25586a6bd8b501784421b59743
nandhinikn1082005bcs27-rgb <nandhinikn1082005@gmail.com>
2026-02-17 12:16:23 +0530

    Create merge_two_list.py

---
+++ merge_two_list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # Dummy node acts as a placeholder
+        dummy = ListNode()
+        curr = dummy
+        
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        
+        # Attach the remaining nodes of whichever list is not empty
+        curr.next = list1 or list2
+        
+        return dummy.next
